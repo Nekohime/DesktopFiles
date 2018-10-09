6e03477227485926a8f96b1da6f212b8a8acdf24
Nekohime <Nekohime@users.noreply.github.com>
2018-10-09 14:14:29 -0400

    Added a RuneLite launcher and installer.

---
+++ setup_runelite.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Downloads RuneLite and installs it in the system, making an icon in the start menu and desktop
+sudo cp runelite.png /usr/share/icons/hicolor/128x128/apps/
+sudo cp runelite.desktop /usr/share/applications/
+cp runelite.desktop ~/Desktop/
+wget https://github.com/runelite/launcher/releases/download/1.6.1/RuneLite.jar
+chmod +x RuneLite.jar
+sudo cp RuneLite.jar /usr/local/bin/
