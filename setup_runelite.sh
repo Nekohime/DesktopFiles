7a553c2c7bec334b690141745a12d70ad85a2a06
Nekohime <Nekohime@users.noreply.github.com>
2018-10-30 19:46:44 -0400

    Removed Scrolls shortcut Modified setup_runelite.sh? Ok

--- setup_runelite.sh
+++ setup_runelite.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # Downloads RuneLite and installs it in the system, making an icon in the start menu and desktop
+rm RuneLite.jar
 sudo cp runelite.png /usr/share/icons/hicolor/128x128/apps/
 sudo cp runelite.desktop /usr/share/applications/
 cp runelite.desktop ~/Desktop/
-wget https://github.com/runelite/launcher/releases/download/1.6.1/RuneLite.jar
+wget https://github.com/runelite/launcher/releases/download/1.6.1/RuneLite.jar RuneLite.jar
 chmod +x RuneLite.jar
 sudo cp RuneLite.jar /usr/local/bin/
