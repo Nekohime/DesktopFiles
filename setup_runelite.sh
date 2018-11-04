232bb15f30f183353d85372b62d6bb44cc5c1a59
Nekohime <Nekohime@users.noreply.github.com>
2018-11-03 20:15:21 -0400

    setup_runelite.sh now downloads the latest RuneLite

--- setup_runelite.sh
+++ setup_runelite.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Downloads RuneLite and installs it in the system, making an icon in the start menu and desktop
-rm RuneLite.jar
 sudo cp runelite.png /usr/share/icons/hicolor/128x128/apps/
 sudo cp runelite.desktop /usr/share/applications/
 cp runelite.desktop ~/Desktop/
-wget https://github.com/runelite/launcher/releases/download/1.6.1/RuneLite.jar RuneLite.jar
+#wget https://github.com/runelite/launcher/releases/download/1.6.1/RuneLite.jar -O RuneLite.jar
+#wget https://github.com/runelite/launcher/releases/latest -O RuneLite.jar
+URL=$(curl -s "https://api.github.com/repos/runelite/launcher/releases/latest" | jq -r '.assets[] | select(.name=="RuneLite.jar") | .browser_download_url')
+curl -LO "$URL"
+
 chmod +x RuneLite.jar
 sudo cp RuneLite.jar /usr/local/bin/
